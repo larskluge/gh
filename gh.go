8a559ba6bb644911828cff859547c63c62ab0b5a
Lars Kluge <l@larskluge.com>
2015-09-27 18:37:51 +0300

    .

---
+++ gh.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"os/exec"
+	"regexp"
+	"strings"
+)
+
+func main() {
+	if _, err := os.Stat(".git"); err == nil {
+		out, _ := exec.Command("git", "config", "remote.origin.url").Output()
+		repo_url := strings.TrimSpace(string(out))
+
+		re := regexp.MustCompile(`github\.com[:\/](.*)\.git$`)
+		match := re.FindStringSubmatch(repo_url)
+
+		if len(match) > 1 {
+			gh_url := fmt.Sprintf("https://github.com/%s", match[1])
+			if _, err := exec.Command("open", gh_url).Output(); err == nil {
+				os.Exit(0)
+			}
+		}
+	}
+
+	os.Exit(1)
+}
