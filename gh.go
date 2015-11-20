6cfa0e858a09cb56b0294abe4bc1365b85ec6f32
Lars Kluge <l@larskluge.com>
2015-11-20 18:05:04 +0700

    Add support for current branch

--- gh.go
+++ gh.go
@@ -12,12 +12,21 @@ func main() {
 	if _, err := os.Stat(".git"); err == nil {
 		out, _ := exec.Command("git", "config", "remote.origin.url").Output()
 		repo_url := strings.TrimSpace(string(out))
-
 		re := regexp.MustCompile(`github\.com[:\/](.*?)(\.git)?$`)
 		match := re.FindStringSubmatch(repo_url)
+		user_and_repo := match[1]
+
+		out, err := exec.Command("git", "symbolic-ref", "--short", "-q", "HEAD").Output()
+		branch_path := ""
+		if err == nil {
+			branch := strings.TrimSpace(string(out))
+			if branch != "master" {
+				branch_path = fmt.Sprintf("/tree/%s", branch)
+			}
+		}
 
 		if len(match) > 1 {
-			gh_url := fmt.Sprintf("https://github.com/%s", match[1])
+			gh_url := fmt.Sprintf("https://github.com/%s%s", user_and_repo, branch_path)
 			if _, err := exec.Command("open", gh_url).Output(); err == nil {
 				os.Exit(0)
 			}
