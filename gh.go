ce938a69b221c09b59ec2355713df9c95a54bb72
Lars Kluge <l@larskluge.com>
2015-12-07 14:12:08 +0700

    Prevent error when git remote is not a github.com url

--- gh.go
+++ gh.go
@@ -14,21 +14,23 @@ func main() {
 		repo_url := strings.TrimSpace(string(out))
 		re := regexp.MustCompile(`github\.com[:\/](.*?)(\.git)?$`)
 		match := re.FindStringSubmatch(repo_url)
-		user_and_repo := match[1]
+		if match != nil {
+			user_and_repo := match[1]
 
-		out, err := exec.Command("git", "symbolic-ref", "--short", "-q", "HEAD").Output()
-		branch_path := ""
-		if err == nil {
-			branch := strings.TrimSpace(string(out))
-			if branch != "master" {
-				branch_path = fmt.Sprintf("/tree/%s", branch)
+			out, err := exec.Command("git", "symbolic-ref", "--short", "-q", "HEAD").Output()
+			branch_path := ""
+			if err == nil {
+				branch := strings.TrimSpace(string(out))
+				if branch != "master" {
+					branch_path = fmt.Sprintf("/tree/%s", branch)
+				}
 			}
-		}
 
-		if len(match) > 1 {
-			gh_url := fmt.Sprintf("https://github.com/%s%s", user_and_repo, branch_path)
-			if _, err := exec.Command("open", gh_url).Output(); err == nil {
-				os.Exit(0)
+			if len(match) > 1 {
+				gh_url := fmt.Sprintf("https://github.com/%s%s", user_and_repo, branch_path)
+				if _, err := exec.Command("open", gh_url).Output(); err == nil {
+					os.Exit(0)
+				}
 			}
 		}
 	}
