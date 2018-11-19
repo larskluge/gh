737afd351774720b737121013d1b4aa44c7bc955
Lars Kluge <l@larskluge.com>
2018-11-19 23:14:00 +0100

    Support Gitlab

--- gh.go
+++ gh.go
@@ -12,10 +12,11 @@ func main() {
 	if _, err := os.Stat(".git"); err == nil {
 		out, _ := exec.Command("git", "config", "remote.origin.url").Output()
 		repo_url := strings.TrimSpace(string(out))
-		re := regexp.MustCompile(`github\.com[:\/](.*?)(\.git)?$`)
+		re := regexp.MustCompile(`(github\.com|gitlab.com)[:\/](.*?)(\.git)?$`)
 		match := re.FindStringSubmatch(repo_url)
 		if match != nil {
-			user_and_repo := match[1]
+			host := match[1]
+			user_and_repo := match[2]
 
 			out, err := exec.Command("git", "symbolic-ref", "--short", "-q", "HEAD").Output()
 			branch_path := ""
@@ -27,7 +28,7 @@ func main() {
 			}
 
 			if len(match) > 1 {
-				gh_url := fmt.Sprintf("https://github.com/%s%s", user_and_repo, branch_path)
+				gh_url := fmt.Sprintf("https://%s/%s%s", host, user_and_repo, branch_path)
 				if _, err := exec.Command("open", gh_url).Output(); err == nil {
 					os.Exit(0)
 				}
